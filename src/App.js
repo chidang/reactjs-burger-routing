f7164989fcb14cdd8177efbf9422c59dc70b1028
Dang Chi <chid@jobready.com.au>
2020-05-08 23:27:12 +0700

    Starting implementation of the burger builder container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containters/BurgerBuilder/BurgerBuilder';
+
 
 function App() {
   return (
     <div>
       <Layout>
-        <div>test</div>
+        <BurgerBuilder></BurgerBuilder>
       </Layout>
     </div>
   );
