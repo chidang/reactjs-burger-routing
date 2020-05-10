7b54ea199fa6b0e4f3903808673f940a47129988
Dang Chi <chid@jobready.com.au>
2020-05-10 11:37:37 +0700

    Creating a Reponsive Sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../SideDrawer/SideDrawer';
 
 const Layout = props => 
     <>
         <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
