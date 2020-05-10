534d04a1e0c84f4f3b5f03993c930ce967c4a8cc
Dang Chi <chid@jobready.com.au>
2020-05-10 12:34:22 +0700

    Working on responsive adjustments

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -4,7 +4,7 @@ import classes from './NavigationItem.module.css';
 const NavigationItem = props => (
   <li className={classes.NavigationItem}>
     <a href={props.link} 
-      className={props.active ? classes.ative : null}>
+      className={props.active ? classes.active : null}>
         {props.children}
     </a>
   </li>
