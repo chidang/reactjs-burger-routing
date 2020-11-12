8636179188dcef425438285ad2a387a1fd19d652
Dang Chi <chid@jobready.com.au>
2020-11-12 13:49:23 +0700

    Outputting the Orders

--- src/containters/Orders/Orders.js
+++ src/containters/Orders/Orders.js
@@ -29,9 +29,14 @@ class Orders extends Component {
 
   render () {
     return (
-      <div>
-        <Order />
-        <Order />    
+      <div> 
+        {this.state.orders.map(order => (
+          <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          price={order.price}
+          />
+        ))}
       </div>
       );
   }
