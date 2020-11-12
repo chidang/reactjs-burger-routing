49e4d4427f23c5a81763b390678c9628b43891cd
Dang Chi <chid@jobready.com.au>
2020-11-12 12:26:38 +0700

    Implementing Navigation Links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containters/BurgerBuilder/BurgerBuilder';
 import Checkout from './containters/Checkout/Checkout';
-import { Route, Switch } from 'react-router-dom';
+import Orders from './containters/Orders/Orders';
+
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
       <Layout>
         <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" exac component={BurgerBuilder} />
         </Switch>
       </Layout>
---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = ( props ) => {
+  
+    return (
+        <div className={classes.Order}>
+            
+        </div>
+    );
+};
+
+export default order;
